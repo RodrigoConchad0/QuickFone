36564a5ca754b2af31c2d6d9889da3ef4b0c5c5c
Rodrigo Conchado <rodrigosconchado@gmail.com>
2024-11-18 22:53:13 +0000

    Tipos De Reparaçao

--- Iphone.php
+++ Iphone.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +7,118 @@
     <?php include('includes/linkscss.php') /* pode ser require */?>
 </head>
 <body>
-    <!-- Menu --->
-    <?php require('includes/menu.php') ?>
-    
+<!-- Menu --->
+<?php require('includes/menu.php') ?>
+
+<section class="services py-5">
+    <div class="container text-center">
+        <div class="row">
+
+            <div class="col-md-2">
+                <a href="Iphone.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Samesung.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                       
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Hawei.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                       
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Xiomi.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                     
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Oppo.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                       
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Outros.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                       
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="services py-5">
+    <div class="container text-center">
+        <div class="row">
+
+            <div class="col-md-2">
+                <a href="Iphone.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Samesung.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                       
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Hawei.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Xiomi.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Oppo.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Outros.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
 
 <!-- Rodape -->
 <?php require('includes/radape.php') ?>
--- Xiaomi.php
+++ Xiaomi.php
@@ -1,18 +1,127 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php include('includes/linkscss.php') /* pode ser require */?>
+    <?php include('includes/linkscss.php') ?>
 </head>
 <body>
     <!-- Menu --->
     <?php require('includes/menu.php') ?>
     
 
+<section class="services py-5">
+    <div class="container text-center">
+        <div class="row">
 
-<!-- Rodape -->
-<?php require('includes/radape.php') ?>
+            <div class="col-md-2">
+                <a href="Iphone.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Samesung.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                       
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Hawei.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                       
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Xiomi.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                     
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Oppo.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                       
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Outros.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                       
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="services py-5">
+    <div class="container text-center">
+        <div class="row">
+
+            <div class="col-md-2">
+                <a href="Iphone.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Samesung.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                       
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Hawei.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Xiomi.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Oppo.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a href="Outros.php" class="text-decoration-none" style="color: black;">
+                    <div class="service-item p-4 bg-light rounded d-flex flex-column align-items-center justify-content-start" style="height: 200px;">
+                        
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <!-- Rodape -->
+    <?php require('includes/radape.php') ?>
 </body>
 </html>
