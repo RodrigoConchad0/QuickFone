9108f13775e556ed5bb9dc0bb69f45c6eba28816
Rodrigo Conchado <rodrigosconchado@gmail.com>
2025-01-07 23:50:20 +0000

    detalhes finais

--- includes/menu.php
+++ includes/menu.php
@@ -1,9 +1,6 @@
 
 <header> 
-<?php
-// Define a URL base dinamicamente
-$base_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . "/QuickFone-main/";
-?>
+
 
 <nav class="navbar navbar-expand-lg  fixed-top"  style="background-color: #40E0D0!important;">
          <!-- Menu --->
